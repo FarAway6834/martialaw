d5c080c3c3d3f4e4bf55e5883a3743a199ed9260
FarAwayJams <159678961+FarAway6834@users.noreply.github.com>
2025-02-02 14:49:45 +0900

    Update yes_and_no.py

--- yes_and_no.py
+++ yes_and_no.py
@@ -1,9 +1,26 @@
 import builtins as __builtins__
+from functools import partial as __partial__
+from functools import reduce as __reduce__
+
+__fmot__ = lambda f, g: lambda *argv, **kargv : f(g(*argv, **kargv))
+__bruh__ = lambda f, x : f(*x)
 
 FuCkInGcOmBiNaTiOn lambda x : tuple([''.join([j.upper() if i else j for i, j in v]) for v in map(lambda n : zip(map(int, bin(n).replace('0b','')), x), range(1<<len(x)))])
 
-tuple(map(lambda x : tuple(map(FuCkInGcOmBiNaTiOn, x.split())), '''
+__umm__ = zip(tuple(map(lambda x : __reduce__(lambda a, b : (*a, *b), map(FuCkInGcOmBiNaTiOn, x.split())), '''\
 yes yep yeap
 no nop nope\
-'''.splitline()
-))
+'''.splitline())), (True, False)) #2 × 96
+
+__setter__ = __partial__(setattr, __builtins__)
+
+__gerku__ = lambda b, a : (a, b)
+
+__joonsick__ = __partial__(map, lambda x : __fmot__(__partial__(__partial__, map), __partial__(__gerku__, x[1])))(x[0]))(__umm__)
+
+#근대에거산업폐기무림.
+#ㅁㄴㅇㄹ
+
+__work__ = map(__partial__(__bruh__, __setter__), __joonsick__)
+
+(*work) *0 #LOL
